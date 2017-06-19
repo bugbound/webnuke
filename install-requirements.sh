75573adede8eff8210ee1d98cdcee8757ad85fee
Mike Minchinton <mike.minchinton@digitalassurance.com>
2017-06-19 22:22:47 +0100

    added script to install requirements

---
+++ install-requirements.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+apt-get install python python-pip git python-selenium -y
+pip install pyvirtualdisplay
+pip install selenium
+pip install ipython
+pip install requests
+apt-get install mitmproxy -y
+wget https://github.com/mozilla/geckodriver/releases/download/v0.15.0/geckodriver-v0.15.0-linux64.tar.gz
+sh -c 'tar -x geckodriver -zf geckodriver-v0.15.0-linux64.tar.gz -O > /usr/bin/geckodriver'
+chmod +x /usr/bin/geckodriver
+rm geckodriver-v0.15.0-linux64.tar.gz
