94f2899d194f9d04151922add379198369107461
John Doe <johndoe@example.com>
2020-04-12 15:34:39 +0100

    added chrome support

--- install-requirements.sh
+++ install-requirements.sh
@@ -3,8 +3,4 @@ apt-get install python python-pip git -y
 pip install pyvirtualdisplay
 pip install selenium
 pip install requests
-wget https://github.com/mozilla/geckodriver/releases/download/v0.16.0/geckodriver-v0.16.0-linux64.tar.gz
-sh -c 'tar -x geckodriver -zf geckodriver-v0.16.0-linux64.tar.gz -O > /usr/bin/geckodriver'
-chmod +x /usr/bin/geckodriver
-rm geckodriver-v0.16.0-linux64.tar.gz
 pip install --upgrade selenium
--- libs/utils/WebDriverUtil.py
+++ libs/utils/WebDriverUtil.py
@@ -1,7 +1,10 @@
 from pyvirtualdisplay import Display
 from selenium import webdriver
 import selenium.webdriver.support.ui as ui
-from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+#from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+
+from selenium.webdriver import Chrome
+
 
 class WebDriverUtil:
 	def __init__(self):
@@ -25,40 +28,26 @@ class WebDriverUtil:
 		if self.debug == False:
 			self.display = Display(visible=0, size=(1920, 1080))
 			self.display.start()
-		profile = self.getWebDriverProfile()
-		profile.set_preference("network.proxy.type", 1)
-		profile.set_preference("network.proxy.http", proxy_host)
-		profile.set_preference("network.proxy.http_port", proxy_port)
-		profile.set_preference("network.proxy.https", proxy_host)
-		profile.set_preference("network.proxy.https_port", proxy_port)
-		profile.set_preference("network.proxy.ssl", proxy_host)
-		profile.set_preference("network.proxy.ssl_port", proxy_port)
-		profile.update_preferences()
 		
-		binary = FirefoxBinary('/usr/bin/firefox-esr')
-		newdriver = webdriver.Firefox(firefox_binary=binary,firefox_profile=profile)
+		#binary = FirefoxBinary('/usr/bin/firefox-esr')
+		#newdriver = webdriver.Firefox(firefox_binary=binary,firefox_profile=profile)
+        
+		chrome_options = webdriver.ChromeOptions()
+		chrome_options.add_argument("--proxy-server=%s"%("http://"+proxy_host+":"+str(proxy_port)))
+		chrome_options.add_argument("--ignore-certificate-errors")
+		driver = Chrome(chrome_options=chrome_options)
+		return driver
 		
-		#newdriver = webdriver.Firefox(firefox_profile=profile)
-		self.wait = ui.WebDriverWait(newdriver, 10) # timeout after 10 seconds
-		return newdriver
 		
 	def getDriver(self, logger):		
 		webnuke_config_proxy_port = 33333
 		webnuke_config_web_api_port = 44444
 		webnuke_config_web_api_url = 'http://localhost:'+str(webnuke_config_web_api_port)
-		#self.web_api_server = WebAPIServer(webnuke_config_web_api_port, logger)
-		#self.web_api_server.startServer()
 		
-		
-		#self.proxy_support = ProxySupport('webnuke-proxy', webnuke_config_proxy_port, webnuke_config_web_api_url)
-		profile = self.getWebDriverProfile()
-		profile.update_preferences()
-
-		binary = FirefoxBinary('/usr/bin/firefox-esr')
-		newdriver = webdriver.Firefox(firefox_binary=binary,firefox_profile=profile)
-		#newdriver = webdriver.Firefox(firefox_profile=profile)
-		#self.driver = self.getDriverWithProxySupport('localhost', webnuke_config_proxy_port)
-		return newdriver
+		chrome_options = webdriver.ChromeOptions()
+		chrome_options.add_argument("--ignore-certificate-errors")
+		driver = Chrome(chrome_options=chrome_options)
+		return driver
 		
 
 
